1c255963adcf6469c6444248d22591634c43ec55
b-bence <borsi92@gmail.com>
2020-02-18 15:02:41 +0100

    HR controller ready

--- controller/hr_controller.py
+++ controller/hr_controller.py
@@ -1,6 +1,7 @@
 from model.hr import hr
 from view import terminal as view
 from model import data_manager
+from datetime import datetime
 
 
 def list_employees():
@@ -10,13 +11,13 @@ def list_employees():
 
 
 def add_employee():
-    all_employees = list_employees()
+    all_employees = data_manager.read_table_from_file(hr.DATAFILE)
     all_employees.append(view.get_inputs(['ID', 'user_name', 'birth_date', 'department', 'clearance']))
     data_manager.write_table_to_file(hr.DATAFILE, all_employees)
 
 
 def update_employee():
-    employees = list_employees()
+    employees = data_manager.read_table_from_file(hr.DATAFILE)
     for index, employee in enumerate(employees, 1):
         print(f'{index}. {", ".join(employee)}')
 
@@ -33,7 +34,7 @@ def update_employee():
 
 
 def delete_employee():
-    employees = list_employees()
+    employees = data_manager.read_table_from_file(hr.DATAFILE)
     for index, employee in enumerate(employees, 1):
         print(f'{index}. {", ".join(employee)}')
 
@@ -44,22 +45,66 @@ def delete_employee():
 
 
 def get_oldest_and_youngest():
-    employees = list_employees()
-
-    view.print_error_message("Not implemented yet.")
+    employees = data_manager.read_table_from_file(hr.DATAFILE)
+    year_index = 2
+    sorted_key = lambda num: (int(num[year_index][:4]), int(num[year_index][5:7]), int(num[year_index][8:10]))
+    sorted_departments = sorted([x for x in employees], key=sorted_key, reverse=False)
+    oldest = sorted_departments[0][1]
+    youngest = sorted_departments[-1][1]
+    print(f'The oldest employee is: {oldest}')
+    print(f'The youngest employee is: {youngest}')
+    print(sorted_departments)
 
 
 def get_average_age():
-    view.print_error_message("Not implemented yet.")
+    current_date = datetime.today().strftime('%Y-%m-%d')
+    date_format = '%Y-%m-%d'
+
+    employees = data_manager.read_table_from_file(hr.DATAFILE)
+    year_index = 2
+    list_of_ages = []
+    for employee in employees:
+        employee_age = datetime.strptime(employee[year_index], date_format)
+        today = datetime.strptime(current_date, date_format)
+        difference = today - employee_age
+        list_of_ages.append(int(difference.days//365.25))
+
+    average = sum(list_of_ages)//len(list_of_ages)
+    print(f' The average age of employees is: {average}')
 
 
 def next_birthdays():
-    view.print_error_message("Not implemented yet.")
+    month_and_days = {'1': 31, '2': 28, '3': 31, '4': 30, '5': 31, '6': 30, '7': 31, '8': 31, '9': 30, '10': 31,
+                      '11': 30, '12': 31}
+    birthday_check = view.get_input('Enter month and day in "mm-dd" format: ')
+
+    month = int(birthday_check.split('-')[0])
+    day = int(birthday_check.split('-')[1])
+
+    input_sum_days = sum(month_and_days[str(i)] for i in range(1, month))
+    input_sum_days += day
+
+    employees = data_manager.read_table_from_file(hr.DATAFILE)
+
+    for employee in employees:
+        employee_month = int(employee[2].split('-')[1])
+        employee_day = int(employee[2].split('-')[2])
+
+        employee_sum_day = sum(month_and_days[str(i)] for i in range(1, employee_month))
+        employee_sum_day += employee_day
+
+        minus_two_weeks = input_sum_days - 14
+        plus_two_weeks = input_sum_days + 14
+        person_name = employee[1]
+
+        if employee_sum_day in range(minus_two_weeks, plus_two_weeks+1):
+            print(person_name)
 
 
 def count_employees_with_clearance():
+    employees = data_manager.read_table_from_file(hr.DATAFILE)
     clearance_index = 4
-    clearances = [people[clearance_index] for people in list_employees()]
+    clearances = [people[clearance_index] for people in employees]
 
     clearance_dict = {}
     for i in clearances:
@@ -73,8 +118,9 @@ def count_employees_with_clearance():
 
 
 def count_employees_per_department():
+    employees = data_manager.read_table_from_file(hr.DATAFILE)
     department_index = 3
-    departments = [people[department_index] for people in list_employees()]
+    departments = [people[department_index] for people in employees]
 
     department_dict = {}
     for i in departments:
