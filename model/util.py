16c8e82735972d16e1207d365302c3b2b876f98f
Dusernajder <dusungulu.tibi@gmail.com>
2020-02-18 19:45:00 +0100

    Generate_id works, needs refactor

--- model/util.py
+++ model/util.py
@@ -2,14 +2,24 @@ import random
 import string
 
 
+def get_combination(items, n):
+    return [random.choice(items) for _ in range(n)]
+
+
 def generate_id(number_of_small_letters = 4,
                 number_of_capital_letters = 2,
                 number_of_digits = 2,
                 number_of_special_chars = 2,
                 allowed_special_chars = r"_+-!"):
 
-    uppercase_letters = string.ascii_uppercase
     lowercase_letters = string.ascii_lowercase
-    numbers = string.digits
+    uppercase_letters = string.ascii_uppercase
+    digits = string.digits
+    special_characters = [char for char in allowed_special_chars]
 
-    return 'T!uq6-b4Yq'
+    arr = get_combination(lowercase_letters, number_of_small_letters)
+    arr += get_combination(uppercase_letters, number_of_capital_letters)
+    arr += get_combination(digits, number_of_digits)
+    arr += get_combination(special_characters, number_of_special_chars)
+    random.shuffle(arr)
+    return ''.join(arr)
